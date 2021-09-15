ad62d96aa354a3e90638edc0dfbc99d759786867
Hinchliffe_Miranda <Hinchliffe_Miranda@G-02-22>
2021-09-15 10:23:47 -0400

    Add project files.

---
+++ understandinglogicinc/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace understandinglogicinc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool beach = true;
+            bool hiking = false;
+            bool city = true;
+
+            bool yourNeeds = (beach && city);
+            bool friendNeeds = (beach || hiking);
+
+            bool tripDecision = (yourNeeds && friendNeeds);
+            Console.WriteLine(tripDecision);
+
+            Console.ReadKey();
+        }
+    }
+}
